5eb5b682fcee275c635e4551c413faebbb02f5fd
Murugan K <murugank0204@gmail.com>
2021-08-28 11:24:34 +0530

    "small correction"

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,6 @@ class MyHomePage extends StatefulWidget {
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
-
 class _MyHomePageState extends State<MyHomePage> {
   String main_area = "";
   String answer_area = "";
